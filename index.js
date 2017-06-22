0849faff8ce1f8d214dbbf3d7f836f3ad0daf6c4
Brian Gonzalez <me@briangonzalez.org>
2017-06-22 14:00:05 -0700

    fix for standard

--- index.js
+++ index.js
@@ -6,26 +6,36 @@
  * See LICENSE file in root directory for full license.
  */
 
-'use strict';
+'use strict'
 
 module.exports = {
   rules: {
     'spread-mixins': require('./lib/rules/spread-mixins'),
+    // because Ember's module system sucks
+    'import/no-extraneous-dependencies': 0,
+    'import/no-unresolved': 0,
+    'import/extensions': 0,
+
+    // stupid rules
+    'no-plusplus': 0,
+
+    // TODO: fix and remove
+    'no-restricted-syntax': 0
   },
   configs: {
     parserOptions: {
       ecmaVersion: 6,
       sourceType: 'module',
       ecmaFeatures: {
-        experimentalObjectRestSpread: true,
-      },
+        experimentalObjectRestSpread: true
+      }
     },
     ember: {
       rules: {
         'dollarshaveclub/spread-mixins': 2,
         'no-underscore-dangle': [2, { allow: ['_super'] }],
-        'consistent-return': 0,
-      },
-    },
-  },
-};
+        'consistent-return': 0
+      }
+    }
+  }
+}
