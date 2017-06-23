381284d0434dc661f7a4d7d5a2751d8c043d6f0e
Brian Gonzalez <me@briangonzalez.org>
2017-06-22 19:32:46 -0700

    brought back plugin

--- index.js
+++ index.js
@@ -1,41 +1,7 @@
-/**
- * @fileoverview Dollar Shave Club's official JavaScript Style Rules.
- *  Linting code to shave the world!
- * @author Arjan Singh <arjan.singh@dollarshaveclub.com>
- * @copyright 2016 Arjan Singh <arjan.singh@dollarshaveclub.com>. All rights reserved.
- * See LICENSE file in root directory for full license.
- */
-
 'use strict'
 
 module.exports = {
-  extends: [
-    'standard',
-    'standard-babel',
-  ],
   rules: {
-    'import/no-extraneous-dependencies': 0,
-    'import/no-unresolved': 0,
-    'import/extensions': 0,
-    'no-plusplus': 0,
-    'no-restricted-syntax': 0,
-    'comma-dangle': [2, 'always-multiline'],
-  },
-  configs: {
-    parserOptions: {
-      ecmaVersion: 7,
-      sourceType: 'module',
-      ecmaFeatures: {
-        experimentalObjectRestSpread: true,
-        modules: true,
-      },
-    },
-    ember: {
-      rules: {
-        'dollarshaveclub/spread-mixins': 2,
-        'no-underscore-dangle': [2, { allow: ['_super'] }],
-        'consistent-return': 0,
-      },
-    },
+    'spread-mixins': require('./lib/rules/spread-mixins'),
   },
 }
